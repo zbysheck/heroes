3bbbc268b106487e9c81cf67aea27523cac05bac
Zbyszek Kisły <zbyshekh@gmail.com>
2015-01-18 21:49:14 +0100

    config added

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/** The name of the database */
+define('DB_NAME', 'herosi');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', '');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** salt, for hashing passwords, 
+feel free to change, but ONLY BEFORE creating any users */
+define('DB_SALT', 'ldbasjdkjsd');
